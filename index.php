99163458f0f724382911612e187239626eeb5d40
ClementMonard <clement.monard@hotmail.fr>
2018-07-25 15:10:17 +0200

    Exercice 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <div class="php">
+      <p>
+        <?php
+            $lastname = 'Monard';
+            $firstname = 'Clément';
+            $age = 21;
+            echo $lastname . $firstname . $age;
+        ?>
+      </p>
+    </div>
+  </body>
+</html>
